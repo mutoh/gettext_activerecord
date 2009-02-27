d515b918037de8a3e96fa658129e55fad762a3e6
Masao Mutoh <mutoh@highway.ne.jp>
2009-02-28 02:12:47 +0900

    Unset create_mofiles options (move it to gettext_rails).

--- lib/gettext_activerecord/tools.rb
+++ lib/gettext_activerecord/tools.rb
@@ -12,16 +12,10 @@ require 'gettext_activerecord'
 require 'gettext_activerecord/parser'
 
 module GetText
+  extend self
 
   alias :create_mofiles_org :create_mofiles
   alias :update_pofiles_org :update_pofiles
-  module_function :create_mofiles_org, :update_pofiles_org
-
-  module_function
-  def create_mofiles(options = {})
-    opts = {:verbose => true, :mo_root => "./locale"}
-    create_mofiles_org(opts)
-  end
 
   def update_pofiles(textdomain, files, app_version, options = {})
     GetText::ActiveRecordParser.init(options)
